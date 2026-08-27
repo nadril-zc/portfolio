b9cb511bf667999d727785479a52106973959950
nadril-zc <nadril.zhc@gmail.com>
2026-08-27 16:33:38 -0400

    Added React Router and proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router";
+import { NavLink } from "react-router";
 import './App.css'
+import './assets/pod1.jpg'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <NavLink to="/" end className={({ isActive }) => isActive ? "active" : ""}>
+          Home
+        </NavLink>
+        <NavLink to="/photography">Photography</NavLink>
+      </nav>
+    </header>
+  )
+}
+
+function Home() {
   return (
     <>
-     
+      <section className="hero">
+          <h1 className="hero__heading">Zack Cerny</h1>
+          <p className="hero__text">Front-End Web Developer</p>
+      </section>
     </>
   )
 }
 
-export default App
+function Photography() {
+  return (
+    <>
+      <section className="photography">
+        <h1>Photography</h1>
+      </section>
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/photography" element={<Photography />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
