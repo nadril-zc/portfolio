44ea64b651cade72689e2bc15fefee645558a25f
nadril-zc <nadril.zhc@gmail.com>
2026-08-28 13:16:13 -0400

    Added photo gallery support, loops through every image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router";
 import { NavLink } from "react-router";
+import useFancybox from './useFancybox';
 import './App.css'
-import './assets/pod1.jpg'
+const imageModules = import.meta.glob("./assets/*.jpg", { eager: true, import: "default" });
 
+const images = Object.entries(imageModules).map(([path, src]) => ({
+  name: path.split("/").pop().split(".")[0],
+  src, // ✅ this is now the resolved URL string, e.g. "/src/assets/ardbeg1-a1b2c3.jpg"
+}));
+
+console.log(images);
 
 function Navbar() {
   return (
@@ -23,17 +30,29 @@ function Home() {
     <>
       <section className="hero">
           <h1 className="hero__heading">Zack Cerny</h1>
-          <p className="hero__text">Front-End Web Developer</p>
+          <p className="hero__text">Front-End Web Developer / Photographer</p>
       </section>
     </>
   )
 }
 
+// Photography section
+
 function Photography() {
+  const [fancyboxRef] = useFancybox({
+    // Your custom options
+  });
+
   return (
     <>
       <section className="photography">
-        <h1>Photography</h1>
+        <div className="photography__photos" ref={fancyboxRef}>
+          {images.map((img) => (
+            <a data-fancybox href={img.src} className="photoThumb">
+              <img src={img.src} alt="" key={img.src} />
+            </a>
+          ))}
+        </div>
       </section>
     </>
   )
